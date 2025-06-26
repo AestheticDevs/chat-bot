ab3eb07f0be6d2cc6caa1fafc4b9c6645ed70084
fajarkusuma46 <fajarkusuma46@gmail.com>
2025-06-26 11:33:15 +0700

    feat: update dependencies and add new UI components
    - Added new dependencies: @radix-ui/react-slot, class-variance-authority, clsx, lucide-react, tailwind-merge, prettier, prettier-plugin-tailwindcss, shadcn, tw-animate-css.
    - Updated TypeScript and Tailwind CSS configurations.
    - Refactored global styles in globals.css to include new color variables and dark mode support.
    - Updated layout.tsx to use Montserrat font and modified metadata for the application.
    - Enhanced the homepage with new content and background images.
    - Redesigned Header component to include navigation links and buttons for Sign In and Register.
    - Added a new button component with variants for different styles.
    - Created utility functions for class name management.
    - Added a components.json file for shadcn configuration.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,36 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <section>
-      <div className="container">
-        <h3 className="text-center text-[42px] font-bold">
+    <section className="max-h-80vh relative place-items-center">
+      <div className="container py-6 text-center">
+        <h3 className="text-[42px] leading-[1.2] font-bold text-slate-800">
           Your{" "}
-          <span className="bg-gradient-to-br to-primary inline-block bg-clip-text text-transparent">
+          <span className="from-secondary-brand to-accent-brand inline-block bg-gradient-to-br bg-clip-text text-transparent">
             Smartest
           </span>{" "}
-          AI Assistant Is Here
+          AI Assistant <br /> Is Here
         </h3>
+        <p className="m-auto mt-3 w-3/4 text-slate-600">
+          Whether you're streamlining support, automating tasks, or just need
+          fast answers—our AI is ready <br /> 24/7 to lighten your load and
+          boost your workflow.
+        </p>
+        <Image
+          alt=""
+          src={"/bg-img-landing.png"}
+          className="absolute top-0 left-0 -z-10 w-full"
+          width={100}
+          height={100}
+          sizes="100"
+        ></Image>
+        <Image
+          alt=""
+          src={"/hero-img.png"}
+          className="w-full"
+          width={400}
+          height={400}
+          sizes="100%"
+        ></Image>{" "}
       </div>
     </section>
   );
--- src/components/public/Header.tsx
+++ src/components/public/Header.tsx
@@ -1,9 +1,12 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+
 export const Header = () => {
   return (
-    <header className="bg-white shadow dark:bg-gray-800 flex">
+    <header className="sticky top-0 z-50 flex backdrop-blur-xs dark:bg-gray-800">
       <div className="container mx-auto px-4 py-4">
-        <div className="flex justify-between items-center">
-          <div className="flex items-center ">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center">
             <a
               href="/"
               className="text-2xl font-bold text-gray-900 dark:text-white"
@@ -11,25 +14,11 @@ export const Header = () => {
               MyApp
             </a>
           </div>
-          <nav className="flex space-x-4">
-            <a
-              href="/"
-              className="text-gray-900 dark:text-white hover:underline"
-            >
-              Home
-            </a>
-            <a
-              href="/about"
-              className="text-gray-900 dark:text-white hover:underline"
-            >
-              About
-            </a>
-            <a
-              href="/contact"
-              className="text-gray-900 dark:text-white hover:underline"
-            >
-              Contact
-            </a>
+          <nav className="flex space-x-1">
+            <Link href="/admin">
+              <Button variant={"accent"}>Sign In</Button>
+            </Link>
+            <Button>Register</Button>
           </nav>
         </div>
       </div>
