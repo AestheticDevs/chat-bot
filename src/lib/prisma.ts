5d0dbe3c6a2c6d8049aaa06824bf0f3949855e54
fajarkusuma46 <fajarkusuma46@gmail.com>
2025-07-22 15:36:48 +0700

    fix prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,15 +1,11 @@
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as {
-  prisma?: PrismaClient;
-};
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-  globalForPrisma.prisma ??
+  globalForPrisma.prisma ||
   new PrismaClient({
-    log: ["query", "error", "warn"],
+    log: ["query", "error", "warn"], // biar keliatan query dan error
   });
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
