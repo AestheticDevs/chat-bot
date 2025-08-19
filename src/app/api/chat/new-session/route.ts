55392cf6a63668b024cf072b48557d572a83d11c
kholishafid <hafidnkholis@gmail.com>
2025-08-20 06:08:49 +0700

    feat: jika sesion masih berlaku, dan menggunakan email yang sama maka dia akan gunakan sesion lama

--- src/app/api/chat/new-session/route.ts
+++ src/app/api/chat/new-session/route.ts
@@ -41,31 +41,105 @@ export async function POST(req: Request) {
       );
     }
 
-    const userAgent = req.headers.get("user-agent") || null;
-
-    const session = await prisma.chat_session.create({
-      data: {
-        name,
-        email,
-        agentId: agent.id,
-        userAgent,
+    const public_user = await prisma.public_users.findFirst({
+      where: {
+        email: email,
       },
     });
 
-    return new Response(
-      JSON.stringify({
-        status: "success",
-        message: "Chat session created.",
-        result: session,
-      }),
-      {
-        status: 200,
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*", // allow all origins
-        },
-      },
+    const now = new Date();
+    const endedAt = new Date(
+      now.getFullYear(),
+      now.getMonth(),
+      now.getDate(),
+      23,
+      59,
+      0,
     );
+
+    if (!public_user) {
+      const public_user = await prisma.public_users.create({
+        data: {
+          name: name,
+          email: email,
+        },
+      });
+
+      const session = await prisma.chat_session.create({
+        data: {
+          agentId: agent.id,
+          publicUserId: public_user.id,
+          endedAt: endedAt,
+        },
+      });
+
+      return new Response(
+        JSON.stringify({
+          status: "success",
+          message: "Chat session created.",
+          result: session,
+        }),
+        {
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*", // allow all origins
+          },
+        },
+      );
+    }
+
+    if (public_user) {
+      const session = await prisma.chat_session.findFirst({
+        where: {
+          agentId: agent.id,
+          publicUserId: public_user.id,
+          endedAt: {
+            gt: now,
+          },
+        },
+      });
+
+      if (!session) {
+        const newSession = await prisma.chat_session.create({
+          data: {
+            agentId: agent.id,
+            publicUserId: public_user.id,
+            endedAt: endedAt,
+          },
+        });
+
+        return new Response(
+          JSON.stringify({
+            status: "success",
+            message: "Chat session created.",
+            result: newSession,
+          }),
+          {
+            status: 200,
+            headers: {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*", // allow all origins
+            },
+          },
+        );
+      }
+
+      return new Response(
+        JSON.stringify({
+          status: "success",
+          message: "Chat session created.",
+          result: session,
+        }),
+        {
+          status: 200,
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*", // allow all origins
+          },
+        },
+      );
+    }
   } catch (err) {
     console.error("[CREATE_CHAT_SESSION]", err);
     return new Response(
